00ef4e916d5723f061bc8406f4234db418993a2b
Destin McMurry <destinmcmurry@gmail.com>
2018-05-03 14:15:56 -0400

    updated form

--- client/components/addNewHH.js
+++ client/components/addNewHH.js
@@ -4,6 +4,7 @@ import history from '../history';
 class AddNew extends Component {
 
   state = {
+    showForm: false,
     img: '',
     coords: '',
     Mstart: '',
@@ -22,20 +23,31 @@ class AddNew extends Component {
     SUend: ''
   }
 
+  // form, once set up, would on submit add hh to database, once set up
+
   render() {
     return (
-      <div className='hhform'>
-        <h4>Want to submit a happy hour?</h4>
-        <p>fill out this form!</p>
-        <label>name</label><input/>
-        <label>location</label><input/>
-        <label>details</label><input/>
-        <br />
-        <small>(just a placeholder for now)</small>
+      <div>
+        <h4>Want to tell us about a happy hour?</h4>
+        <button onClick={() => this.setState({ showForm: true })}>yes!</button>
+        {
+          this.state.showForm &&
+          (
+            <div className='hhform'>
+              <button onClick={() => this.setState({ showForm: false })}>nevermind</button>
+              <label>name</label><input/>
+              <label>location</label><input/>
+              <label>details</label><input/>
+              <br />
+              <small>(just a placeholder for now)</small>
+              <br/>
+              <button>submit</button>
+            </div>
+          )
+        }
       </div>
     )
   }
-
 }
 
 export default AddNew;
